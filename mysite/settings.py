3f3a948e00ee196b43870f5ccb79e2ffee918637
Sumit Kumar <ravi.sumit33@gmail.com>
2020-05-06 16:36:27 +0530

    change for CI

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
