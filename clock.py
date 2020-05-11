6bb544cb4a38b68675932bd38d092b2dd716bae1
Ayush Anshul <ayushanshul07@gmail.com>
2020-05-12 00:07:06 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,10 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from deal_findr.cron import my_cron_job
+import subprocess
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    my_cron_job()
-
+	subprocess.call(('python manage.py my_cron_job'), shell=True, close_fds=True)
 sched.start()
