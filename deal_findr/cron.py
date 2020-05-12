129853350da4ec848120514e5718a48a6b287f9d
Sumit Kumar <ravi.sumit33@gmail.com>
2020-05-12 11:36:25 +0530

    debugging pyppeteer error

--- deal_findr/cron.py
+++ deal_findr/cron.py
@@ -1,9 +1,14 @@
 from deal_findr import models
 from deal_findr.service import serviceStart, notifyDealStatus
 
+logger = logging.getLogger("testlogger")
+
 def my_cron_job():
+    logger.info("Starting cron job")
     deals = models.Deal.objects.all()
+    num_deals = 0
     for deal in deals:
         customer = deal.customer
         serviceStart(customer, deal)
-
+        num_deals += 1
+    self.stdout.write(self.style.SUCCESS('Successfully monitored "%d" deals' % num_deals))
