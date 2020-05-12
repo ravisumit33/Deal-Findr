d6130524a5f17c742d4f0d0c2eb1bfe12620eb13
Sumit Kumar <ravi.sumit33@gmail.com>
2020-05-13 01:27:31 +0530

    fixed nav-links and scroll problem

--- deal_findr/static/deal_findr/js/login.js
+++ deal_findr/static/deal_findr/js/login.js
@@ -1,6 +1,16 @@
+"use strict";
+
+//for highlighting of nav-links
 $(document).ready(function(){
-    $("body").attr("data-offset", $("header").height());
+    $("body").attr("data-offset", $('header').height());
 });
 
-
+//scroll to just below header
+$("a[href^='#']").click(function(e) {
+    e.preventDefault();
+    let target = $(this).attr("href");
+    let offset = $(target).offset();
+    let scrollto = offset.top - $('header').height() + 1; // +1 for nav-link to get highlighted when scrolled
+    $('html, body').animate({scrollTop:scrollto}, 500);
+});
 
